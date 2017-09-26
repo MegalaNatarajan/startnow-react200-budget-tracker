c90da44c1df70f949c4aaf79a8cc6f44d879dbe1
Megala <megalainla@gmail.com>
2017-09-26 10:11:15 -0700

    add

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  context: path.join(__dirname, '/'),
+  context: path.join(__dirname, '/src'),
 
   entry: {
     javascript: './index'
